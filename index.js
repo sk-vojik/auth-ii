51c1c1e38445d37c03b63342f56761d424daa9b5
Scott Vojik <scott.vojik@gmail.com>
2019-02-27 13:43:42 -0800

    login working, token creation functioning as well

--- index.js
+++ index.js
@@ -36,6 +36,54 @@ server.post('/api/register', (req, res) => {
 });
 
 
+//GENERATE TOKEN
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,   //subject is the identity of the token, what its about
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: '1d',
+  }
+
+  return jwt.sign(payload, secret, options);
+}
+
+//LOGIN POST
+server.post('/api/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: `Welcome ${user.username}!`, token})
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+  
+});
+
+
+
+//GET users
+server.get('/api/users', (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+  
+});
+
+
 
 
 
